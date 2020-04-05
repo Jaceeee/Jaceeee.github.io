fc9c368f9ee2f0c0a13e5b552fd48a65c82ec01d
Jace Roldan <jaceroldan0101@gmail.com>
2020-04-05 13:33:37 +0800

    Improve viewports on smaller screens

--- js/home.js
+++ js/home.js
@@ -1,17 +1,13 @@
-console.log('here');
-
 var mini = true;
 
 function toggleSidebar() {
 	var sidebarItems = document.getElementsByClassName('skill-grouping');
 	if (mini) {
-		console.log("opening sidebar");
 		document.getElementById("mySidebar").style.width = "250px";
 		document.getElementById("main").style.marginLeft = "250px";
 		this.mini = false;
 		// toggleSidebarItemVisibility(sidebarItems, true);
 	} else {
-		console.log("closing sidebar");
 		document.getElementById("mySidebar").style.width = "40px";
 		document.getElementById("main").style.marginLeft = "40px";
 		this.mini = true;
@@ -22,7 +18,6 @@ function toggleSidebar() {
 function toggleSidebarItemVisibility(elements, show) {
 	let display = show ? "block" : "None";
 	for (element of elements) {
-		// console.log(element);
 		element.style.display = display;
 		element.style.transition = "0.5s";
 	}
